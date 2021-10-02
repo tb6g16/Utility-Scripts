f1739e75eb5f20ebb8c9cd83469c1e1260ff9009
Thomas Burton <tburton5572@gmail.com>
2021-10-02 12:12:37 +0100

    Changed gcc to compile with c99 standard

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# bash script for compile and running C90 code for both windows and linux
+# bash script for compile and running C99 code for both windows and linux
 
 f_name="main"
 do_print="false"
@@ -28,7 +28,7 @@ do
 done
 
 # also want to change it so the import includes the .c extension
-gcc $f_name.c -o $f_name.exe -ansi -Wall -pedantic -lm
+gcc $f_name.c -o $f_name.exe -std=c99 -Wall -pedantic -lm
 # enable the below two lines and comment the one above to make gcc compile a binary file and MinGW to compile a .exe file
 # gcc $f_name.c -o $f_name -ansi -Wall -pedantic -lm
 # x86_64-w64-mingw32-gcc $f_name.c -o $f_name.exe -ansi -Wall -pedantic
